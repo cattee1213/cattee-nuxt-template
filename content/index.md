6aeda77ce9d314a6dcf6733667c09f6028278e3d
xsf <fei523688393@hotmail.com>
2025-07-22 23:24:15 +0800

    ✨ feat:

---
+++ content/index.md
@@ -0,0 +1,3 @@
+# This is a Content file for the Nuxt application.
+
+# It is used to define the structure and content of the page.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/test-utils',
-    '@nuxt/scripts'
+    '@nuxt/scripts',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate',
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/icon',
+    '@nuxt/content'
   ]
-})
+});
